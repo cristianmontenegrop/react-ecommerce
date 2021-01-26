dd145ce9d9617a85f6c4ddd1be11bc1d252ba9b0
Cris M <cristian.montenegro.p@gmail.com>
2021-01-26 15:33:38 -0800

    feat(f. end): footer, header, product components created. HomeScreen screen created, Bootstrap-react installed, bootswatch css appended

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import products from '../products';
+import Product from '../components/Product';
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((item) => {
+          return (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <Product product={item} />
+              <h3>{item.name}</h3>
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
