c0b9c34818572125b5122188194a2921437c0534
Cris M <cristian.montenegro.p@gmail.com>
2021-01-27 18:08:57 -0800

    feat(frontend): hrefs replaced for LinkContainers in Header Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 import Rating from './Rating';
 import PropTypes from 'prop-types';
@@ -6,15 +7,15 @@ import PropTypes from 'prop-types';
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top'></Card.Img>
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as='div'>
           <Rating value={product.rating} text={`${product.numReviews} reviews`}></Rating>
         </Card.Text>
