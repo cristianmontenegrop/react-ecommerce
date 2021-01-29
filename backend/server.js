63c5e183590066144f189b8f393fbba4cb594115
Cris M <cristian.montenegro.p@gmail.com>
2021-01-28 16:25:31 -0800

    feat(backend): .env file created, npm i dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const products = require('./data/products');
-app.listen(5000, console.log('Server runningon port 5000'));
+const dotenv = require('dotenv');
+
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('API is running...');
@@ -15,3 +19,5 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
