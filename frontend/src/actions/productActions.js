90b84fdfeb30fa03a83266a637bef4bc4eeca1cc
Cris M <cristian.montenegro.p@gmail.com>
2021-02-01 12:10:11 -0800

    feat(frontend): Redux productActions created, redux product stats consumed on HomeScreen.js, Loader.js and Message.js components created

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -8,7 +8,7 @@ import {
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await axios.get('/api/producst');
+    const { data } = await axios.get('/api/products');
 
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
