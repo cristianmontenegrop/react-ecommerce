26c55266c7adfcc5b41a5251013a88d308a1b133
Cris M <cristian.montenegro.p@gmail.com>
2021-02-02 18:11:22 -0800

    feat(backend/userauth): user validation path was created

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -11,10 +12,12 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/products', productRoutes);
-
+app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
@@ -25,5 +28,7 @@ app.use(errorHandler);
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
