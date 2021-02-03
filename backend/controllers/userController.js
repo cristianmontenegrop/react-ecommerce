46b9f0d8c13ba72fafb2f897949bca91b87db501
Cris M <cristian.montenegro.p@gmail.com>
2021-02-02 20:44:05 -0800

    feat(backend): JWT middleware implemented for user authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
 
+import generateToken from '../utils/generateToken.js';
+
 // @desc Auth User & get token
 // @route POST /api/users/login
 // @access Public
@@ -15,7 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
