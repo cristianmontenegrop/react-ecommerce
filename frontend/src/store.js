4c053f5005c0831b6d6e7414ee6756431e13651e
Cris M <cristian.montenegro.p@gmail.com>
2021-02-08 21:28:47 -0800

    feat(frontend): User register Screen created along with update to redux infrastructure and routing

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,7 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers';
-import { userLoginReducer } from './reducers/userReducers';
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
@@ -13,6 +13,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
