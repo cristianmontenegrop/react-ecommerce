7814252da7eac2139f00bf8ca3b8151b67f7e0c6
Cris M <cristian.montenegro.p@gmail.com>
2021-02-09 16:37:56 -0800

    feat(frontend): shipping info now is stored in localStorage and redux state, and ShippingScreen now forwards to /payment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import LoginScreen from './screens/LoginScreen';
 import CartScreen from './screens/CartScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route path='/login' component={LoginScreen}></Route>
+          <Route path='/shipping' component={ShippingScreen}></Route>
           <Route path='/register' component={RegisterScreen}></Route>
           <Route path='/profile' component={ProfileScreen}></Route>
           <Route path='/product/:id' component={ProductScreen}></Route>
