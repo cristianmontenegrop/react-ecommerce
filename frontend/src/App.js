a920005048a52861d12399cd497b676e0949f7ce
Cris M <cristian.montenegro.p@gmail.com>
2021-02-10 19:33:36 -0800

    feat(frontend/orderDetails): orderDetails screen created and displaying order information, also fixed small issue with orderDetailsReducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
           <Route path='/payment' component={PaymentScreen}></Route>
           <Route path='/register' component={RegisterScreen}></Route>
           <Route path='/profile' component={ProfileScreen}></Route>
+          <Route path='/order/:id' component={OrderScreen}></Route>
           <Route path='/product/:id' component={ProductScreen}></Route>
           <Route path='/cart/:id?' component={CartScreen}></Route>
           <Route path='/' exact component={HomeScreen}></Route>
