8ac2e9cdbf0605a20ba4ac848dcfa3df5ffcda5a
Cris M <cristian.montenegro.p@gmail.com>
2021-02-10 16:14:21 -0800

    feat(backend): orderController and orderRoutes created

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,37 @@
+import Order from '../models/orderModel.js';
+import asyncHandler from 'express-async-handler';
+
+// @desc Create new order
+// @route POST /api/orders
+// @access Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethd,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order Items');
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethd,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+  }
+  const createdOrder = await order.save();
+  res.status(201).json(createdOrder);
+});
+
+export { addOrderItems };
