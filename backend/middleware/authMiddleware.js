c70c95d3db51d08434361950f3f20b0ef476c08e
Cris M <cristian.montenegro.p@gmail.com>
2021-02-12 14:53:11 -0800

    feat(backend): user Admin validation middleware created, route GET api/users, accessible only by admin and returning all users

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -28,4 +28,13 @@ const protect = AsyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an Admin');
+  }
+};
+
+export { protect, isAdmin };
