c88df7084cbe1ec6e89047d9034402cefffca195
Cris M <cristian.montenegro.p@gmail.com>
2021-02-11 19:56:10 -0800

    feat(backend): created getMyOrders controller and route to fetch all orders from user

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -74,4 +74,14 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     throw new Error('Payment Data not found');
   }
 });
-export { addOrderItems, getOrderById, updateOrderToPaid };
+
+// @desc Get logged in user orders
+// @route GET /api/orders/myorders
+// @access Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  console.log(req.user._id);
+  const orders = await Order.find({ user: req.user._id });
+  console.log(orders);
+  res.json(orders);
+});
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
