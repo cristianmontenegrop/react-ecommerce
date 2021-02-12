2ead1e6bef9cd4f8b913a69427e6a4b8cd9d06a1
Cris M <cristian.montenegro.p@gmail.com>
2021-02-11 16:23:10 -0800

    feat(font/back): payPal Button installed from react-paypal-button-v2, along with route from back to pass PAYPAL_CLIENT_ID, and funtionality to have the payment method running and updating order payment in database

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
 app.use(notFound);
 
 app.use(errorHandler);
