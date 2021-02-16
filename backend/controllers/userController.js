12f83c5898656a60dfac7c27e06dd71fc522a3ae
Cris M <cristian.montenegro.p@gmail.com>
2021-02-15 18:02:39 -0800

    feat(backend): updateUser and getUserById functionality created along with routing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -112,8 +112,22 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users);
 });
 
+// @desc  Get a user by id
+// @route GET /api/users/:id
+// @access Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 // @desc Delete a user
-// @route DELETE /api/user/:id
+// @route DELETE /api/users/:id
 // @access Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
@@ -127,11 +141,37 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Update user
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 export {
   authUser,
   getUserProfile,
   registerUser,
   updateUserProfile,
   getUsers,
+  getUserById,
   deleteUser,
+  updateUser,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,8 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js';
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
@@ -20,5 +22,10 @@ router
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
 
-router.route('/:id').delete(protect, isAdmin, deleteUser);
+router
+  .route('/:id')
+  .delete(protect, isAdmin, deleteUser)
+  .get(protect, isAdmin, getUserById)
+  .put(protect, isAdmin, updateUser);
+
 export default router;
